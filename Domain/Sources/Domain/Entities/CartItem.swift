6c01bde515bfd4aaa010c47304a30e9ffeff38c3
Balázs Kilvády <bkilvady@gmail.com>
2020-07-07 18:56:38 +0200

    Add id to cart items.

--- Domain/Sources/Domain/Entities/CartItem.swift
+++ Domain/Sources/Domain/Entities/CartItem.swift
@@ -10,9 +10,11 @@ import Foundation
 public struct CartItem {
     public let name: String
     public let price: Double
+    public let id: Int
 
-    public init(name: String, price: Double) {
+    public init(name: String, price: Double, id: Int) {
         self.name = name
         self.price = price
+        self.id = id
     }
 }
