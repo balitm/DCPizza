1c68cfcd0dec9854ad612239b678f76c2e9e8b0e
Balázs Kilvády <bkilvady@gmail.com>
2021-05-07 17:05:18 +0200

    Use id’s in CartItem to make them unique.

--- Domain/Sources/Domain/Repository/Cart.swift
+++ Domain/Sources/Domain/Repository/Cart.swift
@@ -10,31 +10,57 @@ import Foundation
 
 struct Cart {
     static let empty = Cart(pizzas: [], drinks: [], basePrice: 0)
+    private static var _additionNumber = 0
 
     private(set) var pizzas: [Pizza]
     private(set) var drinks: [Drink]
     var basePrice: Double
 
+    private var _ids: [Int]
+    private var _pizzaIds: [Int] { Array(_ids[0 ..< pizzas.endIndex]) }
+    private var _drinkIds: [Int] { Array(_ids[pizzas.endIndex...]) }
+
+    init(pizzas: [Pizza], drinks: [Drink], basePrice: Double) {
+        self.pizzas = pizzas
+        self.drinks = drinks
+        self.basePrice = basePrice
+        Cart._additionNumber = pizzas.count + drinks.count
+        _ids = (0 ..< Cart._additionNumber).map { $0 }
+    }
+
     mutating func add(pizza: Pizza) {
+        _ids.insert(Cart._additionNumber, at: pizzas.count)
         pizzas.append(pizza)
+        Cart._additionNumber += 1
+        assert(_drinkIds.count == drinks.count)
+        assert(_pizzaIds.count == pizzas.count)
     }
 
     mutating func add(drink: Drink) {
+        _ids.append(Cart._additionNumber)
         drinks.append(drink)
+        Cart._additionNumber += 1
+        assert(_drinkIds.count == drinks.count)
+        assert(_pizzaIds.count == pizzas.count)
     }
 
     mutating func remove(at index: Int) {
+        _ids.remove(at: index)
         let count = pizzas.count
         if index < count {
             pizzas.remove(at: index)
         } else {
             drinks.remove(at: index - count)
         }
+        assert(_drinkIds.count == drinks.count)
+        assert(_pizzaIds.count == pizzas.count)
     }
 
     mutating func empty() {
         drinks = []
         pizzas = []
+        _ids = []
+        Cart._additionNumber = 0
     }
 
     var isEmpty: Bool {
@@ -54,44 +80,17 @@ struct Cart {
     }
 
     func items() -> [CartItem] {
-        var items = pizzas.map { pizza in
-            CartItem(name: pizza.name,
-                     price: pizza.ingredients.reduce(basePrice) {
+        var items = pizzas.enumerated().map { pizza in
+            CartItem(name: pizza.element.name,
+                     price: pizza.element.ingredients.reduce(basePrice) {
                          $0 + $1.price
-                     }
-            )
+                     },
+                     id: _pizzaIds[pizza.offset])
         }
-        items.append(contentsOf: drinks.map {
-            CartItem(name: $0.name, price: $0.price)
+        items.append(contentsOf: drinks.enumerated().map {
+            CartItem(name: $0.element.name, price: $0.element.price, id: _drinkIds[$0.offset])
         })
 
         return items
     }
 }
-
-// extension Cart: CombineCompatible {}
-//
-// extension Combinable where Base == Cart {
-//    enum Item {
-//        case pizza(pizza: Pizza)
-//        case drink(drink: Drink)
-//    }
-//
-//    var addItem: AnySubscriber<Item, API.ErrorType> {
-//        AnySubscriber<Item, API.ErrorType>(receiveSubscription: {
-//            DLog("subscription: ", $0)
-//        }, receiveValue: {
-//            switch $0 {
-//            case let .pizza(pizza):
-//                DLog("add pizza: ", pizza.name)
-//                base.add(pizza: pizza)
-//            case let .drink(drink):
-//                DLog("add drink: ", drink.name)
-//                base.add(drink: drink)
-//            }
-//            return .unlimited
-//        }, receiveCompletion: {
-//            DLog("completion: ", $0)
-//        })
-//    }
-// }
