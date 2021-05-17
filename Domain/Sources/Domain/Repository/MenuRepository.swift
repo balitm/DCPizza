2109d5e76b0be5de176f8791fadb90d4e644a487
Balázs Kilvády <bkilvady@gmail.com>
2021-05-17 18:15:03 +0200

    Refine _bag creation.

--- Domain/Sources/Domain/Repository/MenuRepository.swift
+++ Domain/Sources/Domain/Repository/MenuRepository.swift
@@ -20,54 +20,50 @@ struct MenuRepository: MenuUseCase {
     private let _data: Initializer
     private let _imageInfo = PassthroughSubject<ImageInfo, Never>()
     private let _pizzas = PassthroughSubject<PizzasResult, Never>()
-    private var _bag = Set<AnyCancellable>()
+    private let _bag: Set<AnyCancellable>
 
     init(data: Initializer) {
         _data = data
 
         let event = PassthroughSubject<_Event, Never>()
 
-        _data.$component
-            .receive(on: DS.dbQueue)
-            .compactMap {
-                dispatchPrecondition(condition: .onQueue(DS.dbQueue))
-                guard let pizzas = try? $0.get().pizzas else { return nil }
-                return _Event.set(pizzas: pizzas)
-            }
-            .subscribe(event)
-            .store(in: &_bag)
-
-        _imageInfo
-            .subscribe(on: DS.dbQueue)
-            .compactMap { info -> ImageInfo? in
-                dispatchPrecondition(condition: .onQueue(DS.dbQueue))
-                return info.url == nil ? nil : info
-            }
-            .flatMap { info in
-                API.downloadImage(url: info.url!)
-                    .map { _Event.fetched(tuple: _IndexedImage(info.offset, $0)) }
-                    .catch { _ in Empty<_Event, Never>() }
-            }
-            .subscribe(event)
-            .store(in: &_bag)
-
-        event
-            .subscribe(on: DS.dbQueue)
-            .scan(PizzasResult(pizzas: Pizzas.empty)) { (pr: PizzasResult, event: _Event) -> PizzasResult in
-                dispatchPrecondition(condition: .onQueue(DS.dbQueue))
-                switch event {
-                case let .set(pizzas):
-                    return PizzasResult(pizzas: pizzas)
-                case let .fetched(tuple):
-                    var pizzas = pr.pizzas.pizzas
-                    let np = Pizza(copy: pizzas[tuple.offset], image: tuple.image)
-                    pizzas[tuple.offset] = np
-                    return PizzasResult(pizzas: Pizzas(pizzas: pizzas, basePrice: pr.pizzas.basePrice))
+        _bag = [
+            _data.$component
+                .receive(on: DS.dbQueue)
+                .compactMap {
+                    dispatchPrecondition(condition: .onQueue(DS.dbQueue))
+                    guard let pizzas = try? $0.get().pizzas else { return nil }
+                    return _Event.set(pizzas: pizzas)
                 }
-            }
-            .receive(on: DispatchQueue.main)
-            .subscribe(_pizzas)
-            .store(in: &_bag)
+                .subscribe(event),
+
+            _imageInfo
+                .flatMap { info in
+                    API.downloadImage(url: info.url)
+                        .map { _Event.fetched(tuple: _IndexedImage(info.offset, $0)) }
+                        .catch { _ in Empty<_Event, Never>() }
+                }
+                // .debug()
+                .receive(on: DS.dbQueue)
+                .subscribe(event),
+
+            event
+                .subscribe(on: DS.dbQueue)
+                .scan(PizzasResult(pizzas: Pizzas.empty)) { (pr: PizzasResult, event: _Event) -> PizzasResult in
+                    dispatchPrecondition(condition: .onQueue(DS.dbQueue))
+                    switch event {
+                    case let .set(pizzas):
+                        return PizzasResult(pizzas: pizzas)
+                    case let .fetched(tuple):
+                        var pizzas = pr.pizzas.pizzas
+                        let np = Pizza(copy: pizzas[tuple.offset], image: tuple.image)
+                        pizzas[tuple.offset] = np
+                        return PizzasResult(pizzas: Pizzas(pizzas: pizzas, basePrice: pr.pizzas.basePrice))
+                    }
+                }
+                .receive(on: DispatchQueue.main)
+                .subscribe(_pizzas),
+        ]
     }
 
     func pizzas() -> AnyPublisher<PizzasResult, Never> {
