b5a81ab494a00a834e454b2c51872746c45a586c
Balázs Kilvády <bkilvady@gmail.com>
2021-05-17 15:56:17 +0200

    Fix unit test.

--- Domain/Tests/DomainTests/MenuUseCaseTests.swift
+++ Domain/Tests/DomainTests/MenuUseCaseTests.swift
@@ -14,25 +14,26 @@ class MenuUseCaseTests: UseCaseTestsBase {
 
     override func setUp() {
         super.setUp()
-
         service = MenuRepository(data: data)
     }
 
     func testPizzas() {
+        var c: AnyCancellable?
         expectation { expectation in
-            _ = service.pizzas()
+            c = service.pizzas()
                 .first()
                 .sink(receiveValue: {
-                    switch $0 {
-                    case let .failure(error):
+                    if let error = $0.error {
                         XCTAssert(false, "Received error: \(error)")
-                    case let .success(pizzas):
+                    } else {
+                        let pizzas = $0.pizzas
                         DLog("all pizzas: ", pizzas.pizzas.count)
                         XCTAssertGreaterThan(pizzas.pizzas.count, 0)
                     }
                     expectation.fulfill()
                 })
         }
+        c?.cancel()
     }
 
     func testAddPizza() {
